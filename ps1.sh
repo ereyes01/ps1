73cd9a46b0becd359acf7a78f2ab88873b4682df
Eddy Reyes <ereyes@lifesize.com>
2014-05-03 22:32:32 -0500

    Use more standard git rev-parse command to check if in a git tree, cosmetic cleanups.

--- ps1.sh
+++ ps1.sh
@@ -13,31 +13,30 @@ esc_tput()
 
 ps1_branch()
 {
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
 ps1_unstaged()
 {
-    git branch &>/dev/null || return
-    git diff --quiet &> /dev/null || echo -n "*"
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return
+    git diff --quiet &> /dev/null || echo -n '*'
 }
 
 ps1_staged()
 {
-    git branch &>/dev/null || return
-    git diff --quiet --cached &> /dev/null || echo -n "*"
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return
+    git diff --quiet --cached &> /dev/null || echo -n '*'
 }
 
 ps1_untracked()
 {
-    git branch &>/dev/null || return
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return
     git status --porcelain 2> /dev/null | grep -q ^?? && echo -n '*'
 }
 
 ps1_git()
 {
-    prev_rc=$?
-
     # branch
     echo -n $(esc_tput bold)$(esc_tput setaf $cyan)'$(ps1_branch)'$(esc_tput sgr0)' '
 
@@ -54,7 +53,7 @@ ps1_git()
 ps1_rc()
 {
     echo
-    echo -n $(esc_tput bold)$(esc_tput setaf $white)'$?'
+    echo -n $(esc_tput bold)$(esc_tput setaf $white)'$?'$(esc_tput sgr0)
 }
 
 ps1_host_pwd()
@@ -63,13 +62,13 @@ ps1_host_pwd()
     working_dir='\w'
 
     echo -n $(esc_tput bold)$(esc_tput setaf $green)${user_host}$(esc_tput sgr0)' '
-    echo $(esc_tput bold)$(esc_tput setaf $blue)${working_dir}$(esc_tput sgr0)
+    echo -n $(esc_tput bold)$(esc_tput setaf $blue)${working_dir}$(esc_tput sgr0)
 }
 
 ps1_prompt()
 {
     echo
-    echo $(esc_tput bold)$(esc_tput setaf $cyan)'> '
+    echo -n $(esc_tput bold)$(esc_tput setaf $cyan)'> '
 }
 
 export PS1=$(ps1_rc)' '$(ps1_host_pwd)' '$(ps1_git)$(ps1_prompt)$(esc_tput sgr0)
