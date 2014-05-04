0559aa707dc1b1542b7f92aa38b01dcc636b70bc
Eddy Reyes <ereyes@lifesize.com>
2014-05-03 20:07:44 -0500

    Surround all tput output with the special \[ \] shell characters to fix the line length of you command line history. Consequently, all colors have to be pre-evaluated in the PS1 env var, with only the text-generating functions being eval'ed at run time. This new behavior breaks the previous return value functionality.

--- ps1.sh
+++ ps1.sh
@@ -20,6 +20,11 @@ blue='4'
 cyan='6'
 white='7'
 
+esc_tput()
+{
+    echo -n '\['$(tput $*)'\]'
+}
+
 ps1_branch()
 {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
@@ -48,29 +53,29 @@ ps1_git_with_rc()
     prev_rc=$?
 
     # branch
-    echo -n $(tput bold)$(tput setaf $cyan)$(ps1_branch)$(tput sgr0)' '
+    echo -n $(esc_tput bold)$(esc_tput setaf $cyan)'$(ps1_branch)'$(esc_tput sgr0)' '
 
     # any staged files?
-    echo -n $(tput setaf $green)$(ps1_staged)$(tput sgr0)
+    echo -n $(esc_tput setaf $green)'$(ps1_staged)'$(esc_tput sgr0)
 
     # any modified unstaged files?
-    echo -n $(tput setaf $red)$(ps1_unstaged)$(tput sgr0)
+    echo -n $(esc_tput setaf $red)'$(ps1_unstaged)'$(esc_tput sgr0)
 
     # any new untracked files?
-    echo -n $(tput setaf $white)$(ps1_untracked)$(tput sgr0)
+    echo -n $(esc_tput setaf $white)'$(ps1_untracked)'$(esc_tput sgr0)
 
     # on next line, show previous command's return value
     echo
-    echo $(tput bold)$(tput setaf $cyan)${prev_rc}'> '$(tput sgr0)
+    echo $(esc_tput bold)$(esc_tput setaf $cyan)'${prev_rc}> '$(esc_tput sgr0)
 }
 
-ps1_prefix()
+ps1_host_pwd()
 {
     user_host='\u@\h'
     working_dir='\w'
     echo
-    echo -n $(tput sgr0)$(tput bold)$(tput setaf $green)${user_host}$(tput sgr0)' '
-    echo $(tput bold)$(tput setaf $blue)${working_dir}$(tput sgr0)
+    echo -n $(esc_tput bold)$(esc_tput setaf $green)${user_host}$(esc_tput sgr0)' '
+    echo $(esc_tput bold)$(esc_tput setaf $blue)${working_dir}$(esc_tput sgr0)
 }
 
-export PS1=$(ps1_prefix)' $(ps1_git_with_rc)'
+export PS1=$(ps1_host_pwd)' '$(ps1_git_with_rc)$(esc_tput sgr0)
